1a5a200f9984450d4793947a4e92b21de9e910c5
AlyneAlvarenga <alynevs@gmail.com>
2020-02-20 18:55:10 -0500

    added comments to js

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@ superHeroApp.typeOfBattle = $('#battleCondition');
 superHeroApp.battleButton = $('.battleButton');
 superHeroApp.resetButton = $('.resetButton');
 
-//to disappear
+//use them to appear and disappear from the page
 superHeroApp.instructionContainer = $('.instructionContainer');
 superHeroApp.flexContainer = $('.flexContainer'); //all three selects
 
@@ -36,50 +36,55 @@ superHeroApp.powerStats = [
 ];
 
 superHeroApp.player1 = function(){
-superHeroApp.superhero1.on('change', function () {
-    superHeroApp.userSelection1 = superHeroApp.superhero1.val();
+    superHeroApp.superhero1.on('change', function () {
+        //get the value from the user selection to use it in the API call
+        superHeroApp.userSelection1 = superHeroApp.superhero1.val();
 
-    $.ajax({
-        url: 'http://proxy.hackeryou.com',
-        method: 'GET',
-        dataType: 'json',
-        data: {
-            reqUrl: `https://superheroapi.com/api/2827401063977440/${superHeroApp.userSelection1}`
-        }
-    }).then(function (result) {
-        const htmlToAppend = `
-        <div class="resultContainer">
-            <h2>${result.name}</h2>
-            <div class="imgContainer">
-                <img src="${result.image.url}" alt="${result.name}"/>
+        $.ajax({
+            url: 'http://proxy.hackeryou.com',
+            method: 'GET',
+            dataType: 'json',
+            data: {
+                reqUrl: `https://superheroapi.com/api/2827401063977440/${superHeroApp.userSelection1}`
+            }
+        }).then(function (result) {
+            const htmlToAppend = `
+            <div class="resultContainer">
+                <h2>${result.name}</h2>
+                <div class="imgContainer">
+                    <img src="${result.image.url}" alt="${result.name}"/>
+                </div>
+                <div class="circleContainer">
+                    <svg class="svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                        <g class="svgGroup">
+                        <circle class="baseCircle" cx="50" cy="50" r="45" />
+                        <path class="secondCircleHero1" d="
+                                M 50, 50
+                                m -45, 0
+                                a 45,45 0 1,0 90,0
+                                a 45,45 0 1,0 -90,0"></path>
+                        </g>
+                    </svg>
+                    <span class="circleLabel circleLabelHero1"></span>
+                </div>
             </div>
-            <div class="circleContainer">
-                <svg class="svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-                    <g class="svgGroup">
-                    <circle class="baseCircle" cx="50" cy="50" r="45" />
-                    <path class="secondCircleHero1" d="
-                            M 50, 50
-                            m -45, 0
-                            a 45,45 0 1,0 90,0
-                            a 45,45 0 1,0 -90,0"></path>
-                    </g>
-                </svg>
-                <span class="circleLabel circleLabelHero1"></span>
-            </div>
-        </div>
-        `;
-        $('.result1').html(htmlToAppend);
-        superHeroApp.powerStats[0].name = result.name;
-        superHeroApp.powerStats[0].intelligence = result.powerstats.intelligence;
-        superHeroApp.powerStats[0].speed = result.powerstats.speed;
-        superHeroApp.powerStats[0].combat = result.powerstats.combat;
-        superHeroApp.powerStats[0].strength = result.powerstats.strength;
-        })
+            `;
+            //place the result on the page
+            $('.result1').html(htmlToAppend);
+
+            //store the info in our local object, to use later
+            superHeroApp.powerStats[0].name = result.name;
+            superHeroApp.powerStats[0].intelligence = result.powerstats.intelligence;
+            superHeroApp.powerStats[0].speed = result.powerstats.speed;
+            superHeroApp.powerStats[0].combat = result.powerstats.combat;
+            superHeroApp.powerStats[0].strength = result.powerstats.strength;
+            })
     })
 }
 
 superHeroApp.player2 = function(){
     superHeroApp.superhero2.on('change', function () {
+        //get the value from the user selection to use it in the API call
         superHeroApp.userSelection2 = superHeroApp.superhero2.val();
 
         $.ajax({
@@ -111,7 +116,10 @@ superHeroApp.player2 = function(){
                 </div>
             </div>
             `;
+            //place the result on the page
             $('.result2').html(htmlToAppend);
+
+            //store the info in our local object, to use later
             superHeroApp.powerStats[1].name = result.name;
             superHeroApp.powerStats[1].intelligence = result.powerstats.intelligence;
             superHeroApp.powerStats[1].speed = result.powerstats.speed;
@@ -123,25 +131,27 @@ superHeroApp.player2 = function(){
 
 superHeroApp.heroBattle = function(){
     superHeroApp.typeOfBattle.on('change', function () {
-    superHeroApp.userSelection3 = superHeroApp.typeOfBattle.val();
-
-    superHeroApp.powerStats[2].selection = superHeroApp.userSelection3;
+        //get the value from the user selection
+        superHeroApp.userSelection3 = superHeroApp.typeOfBattle.val();
+        //store the info in our local object, to use later
+        superHeroApp.powerStats[2].selection = superHeroApp.userSelection3;
     })
 }
 //*code for svg circle https://itnext.io/how-to-create-an-animated-countdown-timer-with-html-css-and-javascript-d0171d1fb6f7
 
-
+//number for the circumference of the base circle
 superHeroApp.total = 283;
 
+//turn the numbers from the API into a percentage
 superHeroApp.calculatePercentage = function(num){
     return num * superHeroApp.total / 100;
 }
 
 superHeroApp.drawCircles = function(num1, num2) {
-
-const circleDasharray1 = `${(superHeroApp.calculatePercentage(num1).toFixed(0))} 283`;
-    $('.secondCircleHero1').attr('stroke-dasharray', circleDasharray1);
-    $('.circleLabelHero1').append(`${num1}%`);
+    //get the number from the powerstat that was chosen, turn it into a percentage, and draw on top of the initial circle
+    const circleDasharray1 = `${(superHeroApp.calculatePercentage(num1).toFixed(0))} 283`;
+        $('.secondCircleHero1').attr('stroke-dasharray', circleDasharray1);
+        $('.circleLabelHero1').append(`${num1}%`);
 
     const circleDasharray2 = `${(superHeroApp.calculatePercentage(num2).toFixed(0))} 283`;
     $('.secondCircleHero2').attr('stroke-dasharray', circleDasharray2);
@@ -149,6 +159,7 @@ const circleDasharray1 = `${(superHeroApp.calculatePercentage(num1).toFixed(0))}
 }
 
 superHeroApp.whoIsWinner = function() {
+    //check which type of battle the user chose
     const chosenBattle = superHeroApp.powerStats[2].selection;
 
     $('.winnerContainer').append(`
@@ -156,14 +167,17 @@ superHeroApp.whoIsWinner = function() {
         <p>The winner of the battle of ${chosenBattle} is...<p>
             </div>`)
 
+    //pull that specific powerstat from our first object, and convert it to a number
     const hero1 = superHeroApp.powerStats[0][chosenBattle];
     const hero1Num = parseInt(hero1);
 
+    //pull that specific powerstat from our second object, and convert it to a number
     const hero2 = superHeroApp.powerStats[1][chosenBattle];
     const hero2Num = parseInt(hero2);
 
     superHeroApp.drawCircles(hero1Num, hero2Num);
 
+    //determine the winner and print it to the page
     if (hero1Num > hero2Num) {
         $('div.winnerTitle').append(`<h3>${superHeroApp.powerStats[0].name}</h3>`);
 
@@ -175,22 +189,24 @@ superHeroApp.whoIsWinner = function() {
     }
 }
 
+//this triggers the css changes on button click
 superHeroApp.button = function(){
-superHeroApp.battleButton.on('click', function () {
-    superHeroApp.instructionContainer.css('display', 'none');
-    superHeroApp.flexContainer.css('display', 'none');
-    $('.winnerContainer').css('display', 'block');
-    $('.result').css('display', 'flex');
-    if ($(window).width() <= 720) {
-        $('main').css('min-height', '205vh');
-    }
-    superHeroApp.whoIsWinner();
-    superHeroApp.battleButton.css('display', 'none');
-    superHeroApp.resetButton.css('display', 'block');
+    superHeroApp.battleButton.on('click', function () {
+        superHeroApp.instructionContainer.css('display', 'none');
+        superHeroApp.flexContainer.css('display', 'none');
+        $('.winnerContainer').css('display', 'block');
+        $('.result').css('display', 'flex');
+        if ($(window).width() <= 720) {
+            $('main').css('min-height', '205vh');
+        }
+        superHeroApp.whoIsWinner();
+        superHeroApp.battleButton.css('display', 'none');
+        superHeroApp.resetButton.css('display', 'block');
 
-    })
+        })
 }
 
+//this resets the css changes on button click
 superHeroApp.resetButton.on('click', function () {
 
     $('.result1').children().remove();
